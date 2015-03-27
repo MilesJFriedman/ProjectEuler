d559f6ee44a45ff004bae886da1bf8322ef96d93
Miles Friedman <milesfriedmanfl@gmail.com>
2015-03-26 22:38:57 -0700

    almost finished problem 3, need to stop at the largest prime factor and not divide to 1...

--- SimpleProblems.java
+++ SimpleProblems.java
@@ -2,10 +2,13 @@
 public class SimpleProblems {
 	
 	public static void main (String[] args) {
+		double answer = 0;
 		
-		problem1();
+		problem1(); //what is the sum of all multiples of 3 and 5 less than 1000 and greater than 0
 		
-		problem2();
+		problem2(); //what is the sum of all even numbers less than 4000000 in the fib sequence 
+		
+		problem3(961992.0); //what is the largest prime factor of 600851475143?
 		
 		
 	}
@@ -40,14 +43,44 @@ public class SimpleProblems {
 		}
 		
 		System.out.printf("The answer to problem 2 is: %d\n", sum);
+	}
+	
+	 
+	static void problem3(double number) {
+		//define the upper limit for the Sieve
+		int upperLimit = 1000;
 		
-		/*//Recursively iterate through the Fibonacci sequence starting at 4000 and create an
-		//increasing sum to keep track of the sum of all of the fib numbers under 4000
-		if (fib == 1) {
-			return 1;
-		} else if (fib == 2) {
-			return 2;
-		} else 
-			return problem2(fib - 1) + problem2(fib - 2);*/
+		//create an array that will indicate whether or not a number is prime
+		boolean[] isComposite = new boolean[upperLimit];
+		isComposite[1] = true;
+		
+		//Use the Sieve of Eratosthenes to fill the table with composite true values
+		for (int i = 2; i < upperLimit; i++) {
+			for (int j = i; j*i < upperLimit; j++) {
+				isComposite[j*i] = true;
+			}
+			//System.out.println(i + ": " + isComposite[i]);
+		}
+		
+		//Iterate through the isComposite array, testing each prime number to see if the number
+		//is divisible by that prime. If so, pass the new prime number to the recursive function
+		double answer = problem3Rec(number, upperLimit, isComposite);
+		
+		System.out.printf("The answer to problem 3 is: %f", answer);
+	}
+	
+	static double problem3Rec(double number, int upperLimit, boolean[] isComposite) {
+		System.out.println("current number to get prime factor of: " + number);
+		for (int i = 0; i < upperLimit; i++) {
+			//if the current i is a prime number, check to see if it is a prime factor of the
+			//number
+			if (isComposite[i] == false) {
+				if (number % i == 0)
+					return problem3Rec((number/i), upperLimit, isComposite);
+					//break;
+			}
+		}
+		
+		return number;
 	}
 }
